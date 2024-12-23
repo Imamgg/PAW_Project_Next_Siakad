448a26858920b8dc62a15ce9695cfc70cc792d43
Mufid-031 <mufidmaxsus17@gmail.com>
2024-12-23 12:07:19 +0700

    server maintenance

--- frontend/resources/views/admin/dashboard.blade.php
+++ frontend/resources/views/admin/dashboard.blade.php
@@ -13,13 +13,17 @@
                     <table id="logsTable" class="min-w-full divide-y divide-gray-200">
                         <thead class="bg-gray-50">
                             <tr>
-                                <th class="px-6 py-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th
+                                    class="px-6 py-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Tanggal & Waktu</th>
-                                <th class="px-6 py-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th
+                                    class="px-6 py-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Aktifitas</th>
-                                <th class="px-6 py-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th
+                                    class="px-6 py-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Pengguna</th>
-                                <th class="px-6 py-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th
+                                    class="px-6 py-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Email</th>
                             </tr>
                         </thead>
@@ -32,7 +36,8 @@
                                             <p class="text-gray-500" x-text="formatTime(log.createdAt)"></p>
                                         </td>
                                         <td class="px-6 py-4">
-                                            <span class="px-2 inline-flex leading-5 font-semibold" x-text="log.action"></span>
+                                            <span class="px-2 inline-flex leading-5 font-semibold"
+                                                x-text="log.action"></span>
                                         </td>
                                         <td class="px-6 py-4">
                                             <div class="font-medium text-gray-900" x-text="log.user.name"></div>
@@ -57,15 +62,30 @@
                     <div id="paginationControls" class="flex justify-center space-x-2">
                         <template x-for="page in totalPages" :key="page">
                             <button @click="currentPage = page"
-                                :class="{'bg-blue-500 text-white': currentPage === page, 'bg-gray-200 text-gray-700': currentPage !== page}"
-                                class="px-4 py-2 rounded"
-                                x-text="page"></button>
+                                :class="{
+                                    'bg-blue-500 text-white': currentPage ===
+                                        page,
+                                    'bg-gray-200 text-gray-700': currentPage !== page
+                                }"
+                                class="px-4 py-2 rounded" x-text="page"></button>
                         </template>
                     </div>
                 </div>
             </div>
+            <button onclick="toggleMaintenance()"
+                class="px-4 py-2 text-sm font-medium text-white bg-red-600 rounded-md hover:bg-red-700">Server</button>
+
         </div>
         <script>
+            async function toggleMaintenance() {
+                const response = await axios.patch('http://localhost:3000/maintenance/toggle-maintenance');
+                Swal.fire({
+                    icon: "success",
+                    title: "Success",
+                    text: response.data.message
+                })
+            }
+
             function logManagement() {
                 return {
                     logs: @json($logs['data'] ?? []),
@@ -83,10 +103,17 @@
                         return this.logs ? Math.ceil(this.logs.length / this.logsPerPage) : 0;
                     },
                     formatDate(dateString) {
-                        return new Date(dateString).toLocaleDateString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' });
+                        return new Date(dateString).toLocaleDateString('id-ID', {
+                            day: 'numeric',
+                            month: 'long',
+                            year: 'numeric'
+                        });
                     },
                     formatTime(dateString) {
-                        return new Date(dateString).toLocaleTimeString('id-ID', { hour: '2-digit', minute: '2-digit' });
+                        return new Date(dateString).toLocaleTimeString('id-ID', {
+                            hour: '2-digit',
+                            minute: '2-digit'
+                        });
                     }
                 }
             }
--- frontend/resources/views/components/layout.blade.php
+++ frontend/resources/views/components/layout.blade.php
@@ -14,14 +14,27 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}">
     </script>
+    <script src="https://cdn.socket.io/4.6.1/socket.io.min.js"></script>
     <title>Next Siakad</title>
 </head>
 
 <body class="{{ $class }}">
-    {{-- <div class="container mx-auto my-8"> --}}
-
     {{ $slot }}
-    </div>
+    <script>
+        const socket = io('http://localhost:3000/'); // URL backend WebSocket
+
+        socket.on('maintenanceMode', (data) => {
+            if (data.status) {
+                axios.patch('/token/destroy-token')
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Maintenance Mode',
+                    text: 'Sistem sedang dalam perbaikan, silahkan coba lagi beberapa saat lagi.',
+                })
+                window.location.href = '/auth/login';
+            }
+        });
+    </script>
 </body>
 
 </html>
